d0cd6bd2d5d980c90f975e7714a0befd67ce0ba8
Celine Keisja <celinekeisja@gmail.com>
2019-11-11 16:15:00 +0800

    Added Migrate

--- config.py
+++ config.py
@@ -9,10 +9,11 @@ connex_app = connexion.App(__name__, specification_dir=os.path.join(basedir))
 
 app = connex_app.app
 
-postgres_url = 'postgres://127.0.0.1:5433/modulelogs?user=postgres&password=password'
+# postgres_url = 'postgres://127.0.0.1:5433/modulelogs?user=postgres&password=password'
+sqlite_url = "sqlite:////"+os.path.join(basedir, "modulelogs.db")
 
 app.config["SQLALCHEMY_ECHO"] = True
-app.config["SQLALCHEMY_DATABASE_URI"] = postgres_url
+app.config["SQLALCHEMY_DATABASE_URI"] = sqlite_url
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
--- server.py
+++ server.py
@@ -1,4 +1,7 @@
 from flask import Flask
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
+from config import db
 import config
 
 app = config.connex_app
@@ -15,5 +18,9 @@ def job(job_id=""):
     return 'result of job_id'
 
 
+migrate = Migrate(app=app, db=db)
+manager = Manager(app=app)
+manager.add_command('db', MigrateCommand)
 if __name__ == "__main__":
+    manager.run()
     app.run(host='localhost', port=5000, debug=True)
