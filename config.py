9dcb524524418090987a31942065f432cfc040eb
Celine Keisja <celinekeisja@gmail.com>
2019-11-11 17:57:18 +0800

    Update

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ connex_app = connexion.App(__name__, specification_dir=os.path.join(basedir))
 
 app = connex_app.app
 
-postgres_url = 'postgres://127.0.0.1:5433/modulelogs?user=postgres&password=password'
+postgres_url = 'postgres://host.docker.internal:5432/modulelogs?user=postgres&password=password'
 # sqlite_url = "sqlite:////"+os.path.join(basedir, "modulelogs.db")
 
 app.config["SQLALCHEMY_ECHO"] = True
